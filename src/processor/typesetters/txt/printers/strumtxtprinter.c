f6f6e5419b39e6aca85dbd4ddc71651dc9b7db3f
rafael-santiago <voidbrainvoid@gmail.com>
2016-06-06 11:00:43 -0300

    Versioning processor/typesetters/txt/printers/strumtxtprinter.*

---
+++ src/processor/typesetters/txt/printers/strumtxtprinter.c
@@ -0,0 +1,13 @@
+/*
+ *                           Copyright (C) 2005-2016 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
+#include <processor/typesetters/txt/printers/strumtxtprinter.h>
+#include <processor/typesetters/txt/printers/sustainedtxt.h>
+
+void txttypesetter_strum_printer(txttypesetter_tablature_ctx **tab, const tulip_single_note_ctx *note) {
+    txttypesetter_sustained_printer(kTlpStrum, tab, note);
+}
