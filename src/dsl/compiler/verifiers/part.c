107f6eb0b273e9563ded361a14429845d66ea928
rafael-santiago <voidbrainvoid@gmail.com>
2016-06-08 00:54:42 -0300

    Copyright disclaimer

--- src/dsl/compiler/verifiers/part.c
+++ src/dsl/compiler/verifiers/part.c
@@ -1,3 +1,10 @@
+/*
+ *                           Copyright (C) 2005-2016 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
 #include <dsl/compiler/verifiers/part.h>
 #include <dsl/compiler/verifiers/oncemore.h>
 #include <dsl/compiler/compiler.h>
