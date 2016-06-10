f098c287b346447ec332913befd6994ba84d124c
rafael-santiago <voidbrainvoid@gmail.com>
2016-06-10 11:56:34 -0300

    Commenting debug functions

--- src/processor/typesetters/txt/txt.c
+++ src/processor/typesetters/txt/txt.c
@@ -218,6 +218,7 @@ int txttypesetter_eval_buffer_row_usage(const tulip_command_t techniques, const 
     return 0;
 }
 
+/*
 static int show_curr_fretboard(const txttypesetter_tablature_ctx *tab) {
     const txttypesetter_tablature_ctx *tp = tab;
     size_t s = 0;
@@ -236,6 +237,7 @@ static int show_curr_fretboard(const txttypesetter_tablature_ctx *tab) {
     }
     system("read");
 }
+*/
 
 static void cut_data_buf(char *bp) {
     char *bp_end = NULL;
