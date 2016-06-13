69755c6427e2cb4fe2e45d3d554f2f1945bf71ac
rafael-santiago <voidbrainvoid@gmail.com>
2016-06-13 20:53:42 -0300

    Bugfix: .song & .transcriber tags now can alloc the initial tab output

--- src/processor/typesetters/txt/printers/transcribertxtprinter.c
+++ src/processor/typesetters/txt/printers/transcribertxtprinter.c
@@ -6,15 +6,20 @@
  *
  */
 #include <processor/typesetters/txt/printers/transcribertxtprinter.h>
+#include <processor/typesetters/txt/txt.h>
 #include <dsl/str/strutils.h>
 
 void txttypesetter_transcriber_printer(txttypesetter_tablature_ctx **tab, const tulip_single_note_ctx *note) {
     txttypesetter_tablature_ctx *tp = NULL;
-    if (tab == NULL || note == NULL || (*tab) == NULL) {
+    if (tab == NULL || note == NULL) {
         return;
     }
 
-    for (tp = (*tab); tp->last != NULL; tp = tp->last);
+    if ((*tab) != NULL) {
+        for (tp = (*tab); tp->last != NULL; tp = tp->last);
+    } else {
+        tp = txttypesetter_get_properly_output_location(tab, 0);
+    }
 
     tp->transcriber = expand_string(note->buf);
 }
