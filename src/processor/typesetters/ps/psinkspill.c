1d8891afb4c4e83a8681de7b439bb876ef7f0473
rafael-santiago <voidbrainvoid@gmail.com>
2017-02-08 04:09:33 -0900

    Flushing the tunning into the current tab chunk

--- src/processor/typesetters/ps/psinkspill.c
+++ src/processor/typesetters/ps/psinkspill.c
@@ -32,7 +32,7 @@ static void pstypesetter_newpage(FILE *fp);
 
 static void pstypesetter_showpage(FILE *fp);
 
-static void pstypesetter_newtabdiagram(FILE *fp, const int sn);
+static void pstypesetter_newtabdiagram(FILE *fp, const txttypesetter_tablature_ctx *tab);
 
 static void pstypesetter_proc_tabchunk(FILE *fp, const txttypesetter_tablature_ctx *tab);
 
@@ -53,6 +53,8 @@ static void pstypesetter_spill_tab_notation(FILE *fp, const tulip_single_note_ct
 static void pstypesetter_spill_transcribers_name(FILE *fp, const char *transcriber);
 
 static void pstypesetter_spill_song_title(FILE *fp, const char *song);
+
+static void pstypesetter_spill_tunning(FILE *fp, const txttypesetter_tablature_ctx *tab);
 
 static void pstypesetter_vertbar(FILE *fp, const int x, const int y, const int sn);
 
@@ -114,11 +116,12 @@ static void pstypesetter_showpage(FILE *fp) {
                                                 PSTYPESETTER_PAGEY_LIMIT + 20, pn);
 }
 
-static void pstypesetter_newtabdiagram(FILE *fp, const int sn) {
+static void pstypesetter_newtabdiagram(FILE *fp, const txttypesetter_tablature_ctx *tab) {
     int s = 0;
     int sy = 0;
+    struct typesetter_curr_settings cset = typesetter_settings();
 
-    for (s = 0; s < sn; s++) {
+    for (s = 0; s < tab->string_nr; s++) {
         if (pstypesetter_string_y(s, g_ps_ctab.cy) < PSTYPESETTER_PAGEY_LIMIT) {
             pstypesetter_pageinit();
             pstypesetter_showpage(fp);
@@ -129,13 +132,34 @@ static void pstypesetter_newtabdiagram(FILE *fp, const int sn) {
 
     g_ps_ctab.cx = PSTYPESETTER_CARRIAGEX;
 
-    for (s = 0; s < sn; s++) {
+    for (s = 0; s < tab->string_nr; s++) {
         sy = pstypesetter_string_y(s, g_ps_ctab.cy);
         fprintf(fp, "%d %d moveto\n"
                     "%d %d lineto\n", g_ps_ctab.cxl, sy, g_ps_ctab.cxr, sy);
     }
 
     fprintf(fp, "stroke\n");
+
+    if (cset.prefs & kTlpPrefsShowTunning) {
+        pstypesetter_spill_tunning(fp, tab);
+    } else if (cset.prefs & kTlpPrefsFretboardStyleNormal) {
+        pstypesetter_vertbar(fp, g_ps_ctab.cxl, g_ps_ctab.cy, tab->string_nr);
+    }
+}
+
+static void pstypesetter_spill_tunning(FILE *fp, const txttypesetter_tablature_ctx *tab) {
+    size_t s = 0, maxlen = 1, clen = 0;
+
+    for (s = 0; s < tab->string_nr; s++) {
+        if ((clen = strlen(tab->tunning[s])) > maxlen) {
+            maxlen = clen;
+        }
+    }
+
+    for (s = 0; s < tab->string_nr; s++) {
+        fprintf(fp, "%d %d moveto (%s) show\n", g_ps_ctab.cxl - PSTYPESETTER_CARRIAGE_STEP * maxlen,
+                                                pstypesetter_pinch_y(s, g_ps_ctab.cy), tab->tunning[s]);
+    }
 }
 
 static int pstypesetter_string_y(const int sn, const int cy) {
@@ -155,7 +179,7 @@ static void pstypesetter_proc_tabchunk(FILE *fp, const txttypesetter_tablature_c
     pstypesetter_spill_sustained_techniques(fp, tab);
     pstypesetter_spill_times(fp, tab);
 
-    pstypesetter_newtabdiagram(fp, tab->string_nr);
+    pstypesetter_newtabdiagram(fp, tab);
 
     pstypesetter_flush_fretboard_pinches(fp, tab);
 }
@@ -209,14 +233,13 @@ static void pstypesetter_pinch_release_bend(FILE *fp, const int x, const int y, 
 static void pstypesetter_close_tab(FILE *fp, const int x, const int sn) {
     int s = 0, sy = 0;
 
-    /* WARN(Rafael): !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                     -------- HANNA BARBERA's NASTY DICK WAS HERE --------
-                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! >:) Hahaha!!
+    /* WARN(Rafael): !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                     -------- HANNA-BARBERA's DASTARDLY DICK WAS HERE --------
+                     !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! >:) Hahaha!!
     */
 
-    fprintf(fp, "1 1 1 setrgbcolor\n");
-
-    fprintf(fp, "newpath\n"
+    fprintf(fp, "1 1 1 setrgbcolor\n"
+                "newpath\n"
                 "%d %d moveto\n"
                 "%d %d lineto\n"
                 "%d %d lineto\n"
@@ -227,14 +250,13 @@ static void pstypesetter_close_tab(FILE *fp, const int x, const int sn) {
                 "%d %d lineto\n"
                 "%d %d lineto\n"
                 "closepath\n"
-                "fill\n", x, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
-                          x, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10,
-                          g_ps_ctab.cxr, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
-                          g_ps_ctab.cxr, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10,
-                          g_ps_ctab.cxr, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
-                          x, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10);
-
-    fprintf(fp, "0 0 0 setrgbcolor\n");
+                "fill\n"
+                "0 0 0 setrgbcolor\n", x, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
+                                       x, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10,
+                                       g_ps_ctab.cxr, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
+                                       g_ps_ctab.cxr, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10,
+                                       g_ps_ctab.cxr, pstypesetter_string_y(0, g_ps_ctab.cy) + 10,
+                                       x, pstypesetter_string_y(sn - 1, g_ps_ctab.cy) - 10);
 
     pstypesetter_vertbar(fp, x, g_ps_ctab.cy, sn);
 }
@@ -311,7 +333,7 @@ static void pstypesetter_flush_fretboard_pinches(FILE *fp, const txttypesetter_t
                 pstypesetter_close_tab(fp, x, tab->string_nr);
             }
 
-            pstypesetter_newtabdiagram(fp, tab->string_nr);
+            pstypesetter_newtabdiagram(fp, tab);
         }
     }
 
