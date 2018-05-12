efbfe5ae38baae64be98f5cdb17efe4978aa0b66
rafael-santiago <voidbrainvoid@gmail.com>
2018-05-12 06:39:52 -0800

    Version increment
    Patching PS, EPS test vectors due to the new version.

--- src/system/version.c
+++ src/system/version.c
@@ -7,7 +7,7 @@
  */
 #include <system/version.h>
 
-static const char *g_tulip_system_version = "v5";
+static const char *g_tulip_system_version = "v6";
 
 const char *get_tulip_system_version() {
     return g_tulip_system_version;
