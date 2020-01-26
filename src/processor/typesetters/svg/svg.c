a71c3d2a1dd556566712aca63f75fc420634cc1a
rafael-santiago <voidbrainvoid@tutanota.com>
2020-01-26 14:24:00 -0900

    SVG processor: Assuming default paper size instead of exiting with error

--- src/processor/typesetters/svg/svg.c
+++ src/processor/typesetters/svg/svg.c
@@ -13,7 +13,6 @@
 #include <processor/typesetters/typeprefs.h>
 #include <processor/settings.h>
 #include <usrland/cmdlineoptions.h>
-#include <stdio.h>
 
 int svg_typesetter(const tulip_single_note_ctx *song, const char *tabpath) {
     txttypesetter_tablature_ctx *tab = NULL;
@@ -23,8 +22,8 @@ int svg_typesetter(const tulip_single_note_ctx *song, const char *tabpath) {
     extern char *g_svgtypesetter_svg_encoding;
 
     if (typesetter_paper_size(&g_svgtypesetter_page_width, &g_svgtypesetter_page_height) == 0) {
-        fprintf(stderr, "processor ERROR: Invalid paper size.\n");
-        return 0;
+        g_svgtypesetter_page_width = SVGTYPESETTER_DEFAULT_PAGE_WIDTH;
+        g_svgtypesetter_page_height = SVGTYPESETTER_DEFAULT_PAGE_HEIGHT;
     }
 
     g_svgtypesetter_svg_encoding = (char *)get_option("svg-encoding", SVGTYPESETTER_DEFAULT_SVG_ENCODING);
