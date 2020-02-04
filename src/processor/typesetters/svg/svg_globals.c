84a3d2badfde1c057461cbff1af6b22ec47fc44c
rafael-santiago <voidbrainvoid@tutanota.com>
2020-02-04 10:11:05 -0900

    Tracking 'svg_globals.c'

---
+++ src/processor/typesetters/svg/svg_globals.c
@@ -0,0 +1,16 @@
+/*
+ *                                Copyright (C) 2020 by Rafael Santiago
+ *
+ * This is a free software. You can redistribute it and/or modify under
+ * the terms of the GNU General Public License version 2.
+ *
+ */
+#include <processor/typesetters/svg/svgboundaries.h>
+
+int g_svgtypesetter_page_width = SVGTYPESETTER_DEFAULT_PAGE_WIDTH;
+
+int g_svgtypesetter_page_height = SVGTYPESETTER_DEFAULT_PAGE_HEIGHT;
+
+char *g_svgtypesetter_svg_encoding = SVGTYPESETTER_DEFAULT_SVG_ENCODING;
+
+int g_svgtypesetter_svg_embed_font = SVGTYPESETTER_DEFAULT_SVG_EMBED_FONT;
