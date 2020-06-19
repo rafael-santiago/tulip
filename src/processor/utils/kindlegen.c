c606ae1415dd17eafc05e74b9db571853fb80e70
rafael-santiago <voidbrainvoid@tutanota.com>
2020-06-19 12:23:16 -0300

    Using MoveFileEx instead of rename() on Windows
    rename() act like being on Unix-like must use MOVEFILE_REPLACE_EXISTING on Windows and it seems no to use it.

--- src/processor/utils/kindlegen.c
+++ src/processor/utils/kindlegen.c
@@ -9,10 +9,13 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#if defined(_WIN32)
+# include <windows.h>
+#endif
 
 int kindlegen(const char *opf_filepath, const char *outpath) {
     char cmdline[4096], *cp;
-    int has_error;
+    int has_error = 1;
 #if defined(__unix__)
     snprintf(cmdline, sizeof(cmdline) - 1, "kindlegen %s > /dev/null 2>&1", opf_filepath);
 #elif defined(_WIN32)
@@ -30,9 +33,17 @@ int kindlegen(const char *opf_filepath, const char *outpath) {
 
     memcpy(&cmdline[0] + strlen(cmdline), ".mobi\0", 6);
 
+#if defined(__unix__)
     if ((has_error = rename(cmdline, outpath)) != 0) {
         fprintf(stderr, "ERROR: Unable to generate .mobi file.\n");
     }
+#elif defined(_WIN32)
+    if ((has_error = MoveFileEx(cmdline, outpath, MOVEFILE_REPLACE_EXISTING) == 0)) {
+        fprintf(stderr, "ERROR: Unable to generate .mobi file.\n");
+    }
+#else
+# error Some code wanted.
+#endif
 
     return has_error;
 }
